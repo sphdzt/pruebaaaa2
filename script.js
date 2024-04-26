b039b83af1dc21758229329dbb0cbd9fe508cbe9
sphdzt <159202644+sphdzt@users.noreply.github.com>
2024-04-26 10:40:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // Desplazamiento suave al hacer clic en los enlaces de navegación
+    $('a').on('click', function(event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
